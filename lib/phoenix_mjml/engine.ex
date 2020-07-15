9737232e3b282cc43fd799ddf3ae4c76994c675b
Vladimir Dralo <2317652+vladra@users.noreply.github.com>
2020-07-15 12:43:42 +0000

    Make mjml executable path dynamic

--- lib/phoenix_mjml/engine.ex
+++ lib/phoenix_mjml/engine.ex
@@ -2,10 +2,15 @@ defmodule PhoenixMjml.Engine do
   @behaviour Phoenix.Template.Engine
 
   def compile(path, _name) do
-    case System.cmd("mjml", [path, "-s"]) do
+    cmd =
+      case System.get_env("MJML_PATH", nil) do
+        nil -> "mjml"
+        path -> Path.expand(path)
+      end
+
+    case System.cmd(cmd, [path, "-s"]) do
       {html, 0} -> EEx.compile_string(html, engine: Phoenix.HTML.Engine, line: 0)
-      _         -> raise "Error MJML exited with non zero status"
+      _ -> raise "Error MJML exited with non zero status"
     end
   end
-
 end
